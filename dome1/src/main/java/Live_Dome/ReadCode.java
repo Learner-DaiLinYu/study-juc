6c6f80930f94d0c17d6bd9cf572d340495f8cf17
Learner <1181434433@qq.com>
2022-05-21 21:44:32 +0800

    "非公平锁"

--- dome1/src/main/java/Live_Dome/ReadCode.java
+++ dome1/src/main/java/Live_Dome/ReadCode.java
@@ -18,12 +18,30 @@ public class ReadCode {
          * 非公平锁：ABC三个线程 A抢到了锁 BC进入队列 A释放时 D线程来了 之间和B争取，D抢到了，对B不公平
          *
          * acquireQueued：
+         * 对象调用 lock() --> 类方法调用 acquire() ----> 私有静态內部类 AQS的子类的 tryAcquire()
          */
         lock.lock();
-
+        /**
+         * AQS 方法: 尝试加锁，成功就成功 失败则失败
+         * current == getExclusiveOwnerThread()  判断是否可以重入   判断当前线程是否为执行线程
+         * 1.lock方法 必须进行加锁 获取不到 一直等待  涉及同步队列
+         * 2.tryLock  就尝试一次 拿不到就算了
+         *
+         * while + trylock 的方式 对比  lock的方式
+         * 1.CAS操作  没有lock获取锁 加入阻塞队列等操作
+         */
         lock.tryLock();
-
+        /**
+         * 超时获取锁
+         */
         lock.tryLock(0,null);
-
+        /**
+         * 支持可中断获取锁
+         */
+        lock.lockInterruptibly();
+        /**
+         * 解锁  重点复写方法 tryRelease
+         */
+        lock.unlock();
     }
 }
